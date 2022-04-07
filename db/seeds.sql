9fe28119ab0fa4ab1e4d39a2356c28644c413dca
jstable1 <jessica.stabler@gmail.com>
2022-04-07 12:45:46 -0600

    created menu and get functions

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,11 @@
-INSERT INTO department (name)
+INSERT INTO departments (name)
 VALUES
     ('Sales'),
     ('Engineering'),
     ('Finance'),
     ('Legal');
 
-INSERT INTO role (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES
     ('Salesperson', '80000', 1),
     ('Lead Engineer', '150000', 2),
@@ -15,7 +15,7 @@ VALUES
     ('Legal Team Lead', '250000', 4),
     ('Lawyer', '190000', 4);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
     ('Mike', 'Chan', 1, null),
     ('Ashley', 'Rodriguez', 2, null),
--- index.js
+++ index.js
@@ -1,9 +1,86 @@
+const mysql = require('./config/connection');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 
-const mysql = require('./config/connection');
+const createMenu = () =>
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choicesMenu',
+            message: 'What would you like to do?',
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'],
+            validate: choiceMenu => {
+                if (choiceMenu) {
+                    return true;
+                } else {
+                    console.log('What would you like to do?')
+                    return false;
+                }
+            }
+        },
+    ]) .then (answers => {
+        switch (answers.choicesMenu) {
+            case 'View all departments':
+                return viewAllDepts();
+            
+            case 'View all roles':
+                return viewAllRoles();
 
+            case 'View all employees':
+                return viewAllEmployees();
 
+            case 'Add a department':
+                return addDept();
+            
+            case 'Add a role':
+                return addRole();
+            
+            case 'Add an employee':
+                return addEmployee();
+            
+            case 'Update an employee role':
+                return updateEmployee();
+        }
+    });
 
+const viewAllDepts = () => {
+    const sql = `SELECT * FROM departments`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            return;
+        }
+        console.table(['name'], values);
+        });
+};
+
+const viewAllRoles = () => {
+    const sql = `SELECT * FROM roles`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            return;
+        }
+        console.table(['title', 'salary', 'department_id'], values);
+        });
+};
+
+const viewAllEmployees = () => {
+    const sql = `SELECT * FROM employees`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            return;
+        }
+        console.table(['first_name', 'last_name', 'role_id', 'manager_id'], values);
+        });
+}
+
+const addRole = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+        },
+    ]);
+};
+
+createMenu()
 
 
